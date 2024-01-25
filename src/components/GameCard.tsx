f6c0b9b47ae7babc7334248839507bdd4f34fc65
Frank <frankhuang66655@gmail.com>
2024-01-25 15:36:04 +0800

    Add cropped images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,13 +2,14 @@ import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react';
 import { Game } from '../hooks/useGames';
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
+import getCroppedImageUrl from '../services/image-url';
 interface GameCardProps {
   game: Game;
 }
 const GameCard = ({ game }: GameCardProps) => {
   return (
     <Card borderRadius={10} overflow='hidden'>
-      <Image src={game.background_image} alt={game.name} />
+      <Image src={getCroppedImageUrl(game.background_image)} alt={game.name} />
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
         <HStack justifyContent='space-between'>
