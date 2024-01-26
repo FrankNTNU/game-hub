85242fb81205bab42ba4e7c5340c610d05835357
Frank <frankhuang66655@gmail.com>
2024-01-26 14:46:19 +0800

    Refactor: remove duplicate styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,23 +3,29 @@ import { Game } from '../hooks/useGames';
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/image-url';
+import GameCardContainer from './GameCardContainer';
 interface GameCardProps {
   game: Game;
 }
 const GameCard = ({ game }: GameCardProps) => {
   return (
-    <Card width='300px' borderRadius={10} overflow='hidden'>
-      <Image src={getCroppedImageUrl(game.background_image)} alt={game.name} />
-      <CardBody>
-        <Heading fontSize='2xl'>{game.name}</Heading>
-        <HStack justifyContent='space-between'>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
+    <GameCardContainer>
+      <Card width='300px' borderRadius={10} overflow='hidden'>
+        <Image
+          src={getCroppedImageUrl(game.background_image)}
+          alt={game.name}
+        />
+        <CardBody>
+          <Heading fontSize='2xl'>{game.name}</Heading>
+          <HStack justifyContent='space-between'>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+        </CardBody>
+      </Card>{' '}
+    </GameCardContainer>
   );
 };
 
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,6 +3,7 @@ import apiClient from '../services/api-client';
 import { SimpleGrid, Text } from '@chakra-ui/react';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
+import GameCardContainer from './GameCardContainer';
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
@@ -15,9 +16,15 @@ const GameGrid = () => {
         spacing={10}
         padding={10}>
         {isLoading &&
-          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
